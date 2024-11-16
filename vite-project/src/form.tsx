5e5734d1e5b1232edc24006ff0927cebc921fdfa
DaviPTeles <davipereirateles7@gmail.com>
2024-11-15 23:12:26 -0400

    Initial commit

---
+++ vite-project/src/form.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+export function Form() {
+    return (
+      <main>
+        <header>
+          <h1>Cadastrar senha</h1>
+        </header>
+        <form action="">
+          <input type="text" />
+          <input type="text" />
+          <button>Cadastrar</button>
+        </form>
+        <footer>
+            <p>Curso desenvolvimento web</p>
+        </footer>
+      </main>
+    )
+  }
